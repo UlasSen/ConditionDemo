9370df1999dc835d001b9111fdad0e39215fc6f4
Dionachil <ulasennn@gmail.com>
2024-02-18 22:48:33 +0300

    Add project files.

---
+++ ConditionDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿int sayi1 = 15;
+int sayi2 = 30;
+int sayi3 = 3;
+
+//Find the largest number
+int largestNumber = sayi1;
+if (largestNumber < sayi2)
+{
+    largestNumber = sayi2;
+}
+if (largestNumber < sayi3) {  largestNumber = sayi3; }
+Console.WriteLine("Largest number is "+largestNumber);
